187bb2b10068978d6289650929e599d78afdb54d
Eric Olaveson <olavesoneric@gmail.com>
2019-03-23 16:24:11 -0500

    start of the end

---
+++ app.js
@@ -0,0 +1,5 @@
+//cache the dom is numero uno
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
